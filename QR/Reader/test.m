bdafaff9cba2fa4895f4c5c15ab93defade80004
Darka <darka@Darka.homenet.telecomitalia.it>
2015-04-17 08:34:11 +0200

    +add data reader +add more example

--- QR/Reader/test.m
+++ QR/Reader/test.m
@@ -1,26 +1,47 @@
 qr=imread('QRCode.png');
-qr = crop_QR(qr);
-
-tx=1;
-ty=1;
-
-while qr(tx,ty)==0
-    tx=tx+1;
+try
+qr=rgb2gray(qr);
+catch
 end
-%modulo length
-ml=(tx-1)/7;
+qr = uint8(ones(size(qr)))-qr;
+qr_matrix = get_matrix_from_image(qr);
+[ecl, mask_n, version]=decode_format_information(qr_matrix);
 
-tx = ml;
-ty = ml;
+qr_matrix = release_masking(qr_matrix, mask_n);
 
-qr_matrix = ones(size(qr)/ml);
+%%%%%%%%%%%% Mettere in una funzione %%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%value for pointless bit
+global null;
+null = -1;
+side = size(qr_matrix,1);
+%qr_matrix = qr_matrix*255;
+%%Remove position detection pattern (i 3 quadratoni)
+%remove up left
+qr_matrix([1:9],[1:9]) = null;
+%remove up right
+qr_matrix([1:9],[side-7:side])=null;
+%remove down left
+qr_matrix([side-7:side], [1:9])=null;
 
-for tx=ml:ml:size(qr,1)
-    for ty=ml:ml:size(qr,2)
-        %mean of modulo for get the color
-        modulo_color=mean2(qr([tx-ml+1:tx],[ty-ml+1:ty]));
-        qr_matrix(tx/ml,ty/ml)=qr_matrix(tx/ml,ty/ml)-modulo_color;
+%%remove timing patterns (le linee)
+qr_matrix(7,:) = null;
+qr_matrix(:,7) = null;
+
+%%removing allignement patterns
+if version >1
+    centers = get_allignment_patterns_center(version);
+    %if the centers are more then one TODO:add for more then 7
+    for c=centers.list
+        qr_matrix([side-6-2:side-6+2],[c+1-2:c+1+2]) = null;
     end
 end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+data = retrieve_data (qr_matrix);
 
-        
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% %Print result
+% imshow(uint8(qr_matrix)*255);
+% figure, imshow(qr*255);
