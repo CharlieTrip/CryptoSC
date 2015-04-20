863d626df56fcca75b34f2a9e41240865298c076
MARIyuya <yu-ya@hotmail.it>
2015-04-20 19:29:28 +0200

    Modifiche alle funzioni contaCambi e orientaDM

--- DM/Reader/orientaDM.m
+++ DM/Reader/orientaDM.m
@@ -1,15 +1,17 @@
-%input: m=matrice non orientata
-
-%orienta la matrice rispettando la sincronizzazione delle DM
-%cioè in tutti gli angoli ci sono 1, mentre in alto a destra 0 
-
-%output: a=matrice orientata
+%
+% input: m=matrice non orientata
+%
+% orienta la matrice rispettando la sincronizzazione delle DM
+% cioè in tutti gli angoli ci sono 1, mentre in alto a destra 0 
+%
+% output: a=matrice orientata
+%
 
 function [a] = orientaDM(m)
 
 	l=length(m);
-
 	a=zeros(l,l);
+
 
 	%caso 1: lo 0 è nell'angolo in alto a sinistra
 
@@ -21,6 +23,7 @@ function [a] = orientaDM(m)
 		end;
 	end;
 
+
 	%caso 2: lo 0 è nell'angolo in basso a sinistra
 
 	if  (m(l,1)==0)  
@@ -31,6 +34,7 @@ function [a] = orientaDM(m)
 		end;
 	end;
 
+
 	%caso 3: lo 0 è nell'angolo in basso a destra
 
 	if  (m(l,l)==0)  
@@ -39,6 +43,13 @@ function [a] = orientaDM(m)
 				a(l-j+1,i)=m(i,j);
 			end;
 		end;
+	end;
+
+
+	%caso 4: lo 0 è nell'angolo in alto a destra
+
+	if (m(1,l)==0)
+		a=m;
 	end;
 
 end
