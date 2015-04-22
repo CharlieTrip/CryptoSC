e6e83959d1b0ead7fb5a688245be06f954cb5ced
mello29 <alessandro.melloni.29@gmail.com>
2015-04-22 11:16:27 +0200

    Standard matrix form function

---
+++ DM/Generator/StdMatForm.m
@@ -0,0 +1,17 @@
+% 
+% MsgToMat
+% Function to place the message into the matrix form
+% 
+% Input
+% 	msg		: message
+% 
+% Output
+%	mat		: matrix form of the message
+
+function mat = StdMatForm(msg)
+
+% Standard codeword placement
+            |1|2|
+            |3|4|5|
+            |6|7|8|
+end
