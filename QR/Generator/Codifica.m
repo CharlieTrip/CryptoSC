72289eb945cde8cdea66e674623cc763da6abbd9
Alessandro Budroni <alessandro.budroni@studenti.unitn.it>
2015-04-25 16:33:17 +0200

    Aggiornata Codigica.m

--- QR/Generator/Codifica.m
+++ QR/Generator/Codifica.m
@@ -12,6 +12,8 @@ function ArrayByte = Codifica(Stringa,Version,mode,EClevel)
 
 if mode == [0,0,1,0] %  TODO aggiungere gli altri mode
     ArrayBit = alfa_numeric(Stringa);
+elseif mode == [0,0,0,1]
+    ArrayBit = numerica(Stringa,Version);
 end
 
 % Padding e Terminator
@@ -35,10 +37,10 @@ while length(ArrayBit) < numDataBits
     ArrayBit = cat(2,ArrayBit,[0,0,0,1,0,0,0,1]);  
 end
 
-tmpt = transpose(reshape(ArrayBit,8,numDataBits/8))
+tmpt = transpose(reshape(ArrayBit,8,numDataBits/8));
 
 
-ArrayByte = [];
+ArrayByte = {};
 if  n_block2 == 0
     for i = 1:n_block1
         ArrayByte(1:n_data_block1,:,i) = tmpt(1:n_data_block1,:);
@@ -46,15 +48,17 @@ if  n_block2 == 0
     end
 else
     for i = 1:n_block1
-        ArrayByte(1:n_data_block1,:,i) = tmpt(1:n_data_block1,:);
+        ArrayByte1(1:n_data_block1,:,i) = tmpt(1:n_data_block1,:);
         tmpt(1:n_data_block1,:) = [];
     end
-    for i = n_block1+1:n_block1+n_block2
-        ArrayByte(1:n_data_block2,:,i) = tmpt(1:n_data_block2,:);
+    for i = 1:n_block2
+        ArrayByte2(1:n_data_block2,:,i) = tmpt(1:n_data_block2,:);
         tmpt(1:n_data_block2,:) = [];
     end
 end
 
+ArrayByte = [ArrayByte1,ArrayByte2];
+
 
 
 
@@ -62,7 +66,7 @@ end
 
 
 
-
+% TODO da sistemare
 
 
 function ArrayBit = alfa_numeric(Stringa)
@@ -99,6 +103,38 @@ end
 end
 
 
+
+%% prende in input una stringa di numeri e la versione e la codifica nel mode numerico.
+%% poi bisognera fare ancora il padding
+
+
+function ArrayByte = numerica(Stringa,version)
+parity = mod(length(Stringa),3); %resto mod 3
+lenStringa = (length(Stringa)-parity); % lunghezza stringa divisibile per tre
+keyset = ['0', '1' ,'2','3','4','5','6','7','8','9'];
+if version < 10
+ArrayByte = cat(2,[0,0,0,1],de2bi(length(Stringa),10,'left-msb'));
+elseif  and(version >= 10, version < 27)
+    ArrayByte = cat(2,[0,0,0,1],de2bi(length(Stringa),12,'left-msb'));
+else
+    ArrayByte = cat(2,[0,0,0,1],de2bi(length(Stringa),14,'left-msb'));
+end
+%devo trasformare 3 a 3 la stringa in interi
+s= floor(lenStringa/3);
+for i= 1 : s
+    num = (find(keyset == Stringa(3*(i-1)+1))-1)*100 +(find(keyset == Stringa(3*(i-1)+2))-1)*10 +(find(keyset == Stringa(3*(i-1)+3))-1);
+    ArrayByte=cat(2,ArrayByte,de2bi(num,10,'left-msb'));
+end
+ if parity == 2
+     num = (find(keyset == Stringa(length(Stringa)-1 ))-1 )*10 + find(keyset == Stringa(length(Stringa)))-1;
+    ArrayByte=cat(2,ArrayByte,de2bi(num,7,'left-msb'));
+ end
+ if parity == 1
+     num = find(keyset == Stringa(length(Stringa)))-1;
+     ArrayByte=cat(2,ArrayByte,de2bi(num,4,'left-msb'));
+end
+
+end
 
 
 
