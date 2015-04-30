0c5c8f7694898117f3217a7dca43f7deffd6605f
riemann89 <giu.giffone@tiscali.it>
2015-04-30 15:34:20 +0200

    Eliminazione file RSEncoder e funzione RSEncoder in Encoding

--- QR/Generator/Encoding.m
+++ QR/Generator/Encoding.m
@@ -76,53 +76,6 @@ end
 end
 
 
-
-
-
-
-
-% Function to Encode a list of integers in 0..255 using
-% Reed-Solomon with parameter n,k
-% 
-% Input
-% 	n 		: length of the codeword
-% 	k 		: dimension of the message
-% 	comeMsg : list that rappresent the message
-% 
-% Output
-%	EncodeMsg ; encoded message of length n
-function encodedMsg = RSEncoder(codedMsg, n, k)
-
-	% Creating the Encoder using the Matlab's built-in function
-	encoder = comm.RSEncoder(n, k);
-	
-
-	% Enable the possibility to change the parameter of the encoder	release(encoder)
-	
-	% Changing the Primitive Polynomial of the encoder
-	% For DataMatrix, the poly is
-	% 
-	% The function de2bi is in Big Endian so we flip the list to get a Little Endian
-	encoder.PrimitivePolynomialSource = 'Property';
-	encoder.PrimitivePolynomial = fliplr(de2bi(285,9));
-
-	% Changing the Generator Polynomial of the RS code
-	% with respect to the 301 poly for using element of GF(256)
-	encoder.GeneratorPolynomialSource = 'Property';
-	encoder.GeneratorPolynomial = rsgenpoly(255,255 - (n-k),285,0);
-
-
-	% Return the encoding (using step) and traspose it to a row (using ')
-	encodedMsg = step(encoder, codedMsg')';
-	
-end
-
-
-
-
-
-
-
 % Input: 
 %       version --> int, the right version of QR to use
 %       ecl --> char, 'H','Q','M','L'
